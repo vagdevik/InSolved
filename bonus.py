0c23463a539b83b2991eaf68c180c1beb999ba7a
vagdevik <30801221+vagdevik@users.noreply.github.com>
2018-02-25 11:45:24 +0530

    Bonus

---
+++ bonus.py
@@ -0,0 +1,57 @@
+import os
+import humanize
+
+def get_size(filename):
+    st = os.stat(filename)
+    return st.st_size
+
+def list_files(dir):
+	r = []
+	subdirs = [x[0] for x in os.walk(dir)]
+	for subdir in subdirs:
+		files = os.walk(subdir).next()[2]
+		if (len(files) > 0):
+			for file in files:
+				r.append(subdir + "/" + file)
+	return r
+
+def get_folderwise_sizes(list_of_directories):
+	global folder_sizes_in_bytes, folder_sizes
+	folder_sizes_in_bytes = []
+	folder_sizes = []
+	for folder in list_of_directories:
+		size = 0
+		dir_files = list_files(folder)
+		for f in dir_files:
+			size += get_size(f)
+		folder_sizes_in_bytes.append(size)
+		size_in_MB = humanize.naturalsize(size)
+		folder_sizes.append(size_in_MB)
+	print folder_sizes
+
+def get_typewise_sizes(list_of_directories):
+	global out
+	out = []
+	for folder in list_of_directories:
+		files_type_sizes = {}
+		dir_files = list_files(folder)
+		for f in dir_files:
+			if len(f.split('/')[-1].split('.')) != 1:
+				extension = f.split('.')[-1]
+				if not extension in files_type_sizes and extension != 'desktop' :
+					files_type_sizes[str(extension)] = 0
+				if extension != 'desktop':
+					files_type_sizes[str(extension)]+=get_size(f)
+		out.append(files_type_sizes)
+	for folder in out:
+		for typ in folder:
+			folder[typ] = humanize.naturalsize(folder[typ])
+	count = 0
+	for folder in out:
+		print 'folder:  ' + str(list_of_directories[count])
+		for typ in folder:
+			print typ + ' ' + str(folder[typ])
+	
+list_of_directories = ['Desktop']
+get_folderwise_sizes(list_of_directories)
+get_typewise_sizes(list_of_directories)
